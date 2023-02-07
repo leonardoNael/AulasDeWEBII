fcaea91e5a596a5c1a5531f4647035e8872e4d50
leonardoNael <47906698+leonardoNael@users.noreply.github.com>
2023-02-07 02:57:07 -0300

    tudo menos o index ta bão (y)
    carrossel manual funcionando, falta lógica para a ordem das imagens

--- Aula0202/js/java.js
+++ Aula0202/js/java.js
@@ -1,11 +1,34 @@
 var anterior = 0;
 var anteriorP = 0;
-var proxima = 0;
-var imgs = ["<img src='/img/bike.jpeg' />", "<img src='img/violao.jfif' />", "<img src='img/bike.jpeg' />"];
+var proxima = 4;
+var proximaP = 4;
+var imgs = ["img/bike.jpeg", "img/violao.jfif", "img/carro.jfif", "img/mae.jpg", "img/pai.jfif"];
 
 function imganterior(){
     while(anterior < (anteriorP+1)){
         document.getElementById("imgopc").src = imgs[anterior];
+        document.getElementById("imgopc").style.width = "300px";
         anterior++;
     }
+
+    anteriorP++;
+    if (anteriorP > 4){
+        anteriorP = 0;
+        anterior = 0;
+    }
+
 }
+
+function proximg(){
+    while(proxima > (proximaP-1)){
+        document.getElementById("imgopc").src = imgs[proxima];
+        document.getElementById("imgopc").style.width = "300px";
+        proxima--;
+    }
+    proximaP--;
+    if (proximaP < 0){
+        proximaP = 4;
+        proxima = 4;
+    }
+}
+
